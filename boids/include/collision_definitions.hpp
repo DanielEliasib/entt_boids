23e7cd6917685273bd86a2763660c6c1cd5ec5c1
DanielEliasib <Daniel.Eliasib.AL@outlook.com>
2023-10-30 23:30:07 -0600

    Collision cloned cleaned. Collisions bug fixed, now collisions return the closest point not the first one in the collision view. Added collision walls for screen and random block for collision.

--- boids/include/collision_definitions.hpp
+++ boids/include/collision_definitions.hpp
@@ -12,19 +12,18 @@ struct rect_collider : collider {
 
     rect_collider(bool is_trigger, Vector2 extents)
         : collider{is_trigger}, extents{extents} {}
+
+    void generate_conners(std::vector<Vector2> &corners) {
+        corners[0] = Vector2{-extents.x / 2, -extents.y / 2};
+        corners[1] = Vector2{extents.x / 2, -extents.y / 2};
+        corners[2] = Vector2{extents.x / 2, extents.y / 2};
+        corners[3] = Vector2{-extents.x / 2, extents.y / 2};
+    }
 };
 
 struct circle_collider : collider {
     float radius;
 };
-
-static void generate_rect_conners(Vector2 extents,
-                                  std::vector<Vector2> &corners) {
-    corners[0] = Vector2{-extents.x / 2, -extents.y / 2};
-    corners[1] = Vector2{extents.x / 2, -extents.y / 2};
-    corners[2] = Vector2{extents.x / 2, extents.y / 2};
-    corners[3] = Vector2{-extents.x / 2, extents.y / 2};
-}
 
 static bool raycast(entt::registry &registry, Vector2 origin, Vector2 direction,
                     float distance = 500, Vector2 *collision_point = nullptr) {
@@ -32,37 +31,55 @@ static bool raycast(entt::registry &registry, Vector2 origin, Vector2 direction,
     auto rect_collider_view = registry.view<transform, rect_collider>();
     direction = Vector2Normalize(direction);
 
+	//? should we reserve some space?
+	std::vector<Vector2> hit_points;
+
     for (auto [entity, transform, collider] : rect_collider_view.each()) {
         float angle = atan2(transform.direction.y, transform.direction.x);
+        Matrix rotation_matrix = MatrixRotateZ(angle);
 
-        auto RotationMatrix = MatrixRotateZ(angle);
-        Vector2 rotated_identity =
-            Vector2Transform(Vector2{1, 0}, RotationMatrix);
-        auto TranslatedMatrix =
+        Matrix translation_matrix =
             MatrixTranslate(transform.position.x, transform.position.y, 0);
-        auto FullMatrix = MatrixMultiply(TranslatedMatrix, RotationMatrix);
+        Matrix transform_matrix =
+            MatrixMultiply(rotation_matrix, translation_matrix);
 
-        BoundingBox box = {Vector3{0 - collider.extents.x / 2,
-                                   0 - collider.extents.y / 2, -10},
-                           Vector3{0 + collider.extents.x / 2,
-                                   0 + collider.extents.y / 2, 10}};
+        //? Should this BB be generated once and stored?
+        BoundingBox box = {
+            Vector3{0 - collider.extents.x / 2, 0 - collider.extents.y / 2, -1},
+            Vector3{0 + collider.extents.x / 2, 0 + collider.extents.y / 2, 1}};
 
-		Vector2 relative_origin = Vector2Subtract(origin, transform.position);
-		Vector3 ray_origin = Vector3Transform(Vector3{relative_origin.x, relative_origin.y, 0}, MatrixInvert(RotationMatrix));
-		Vector3 ray_direction = Vector3Transform(Vector3{direction.x, direction.y, 0}, MatrixInvert(RotationMatrix));
+        //! Relative operations in the rect space
+        Matrix inverse_rotation_matrix = MatrixInvert(rotation_matrix);
+        Vector2 relative_origin = Vector2Subtract(origin, transform.position);
+        Vector3 ray_origin =
+            Vector3Transform(Vector3{relative_origin.x, relative_origin.y, 0},
+                             inverse_rotation_matrix);
+
+        Vector3 ray_direction = Vector3Transform(
+            Vector3{direction.x, direction.y, 0}, inverse_rotation_matrix);
 
         Ray ray = {ray_origin, ray_direction};
         auto hit = GetRayCollisionBox(ray, box);
 
         if (hit.hit) {
-            auto hit_point =
-                Vector3Transform(Vector3{hit.point.x, hit.point.y, 0}, RotationMatrix);
-			hit_point = Vector3Transform(hit_point, TranslatedMatrix);
-            if (collision_point != nullptr) {
-                *collision_point = Vector2{hit_point.x, hit_point.y};
-            }
-            return true;
+            auto hit_point = Vector3Transform(hit.point, transform_matrix);
+			hit_points.push_back(Vector2{hit_point.x, hit_point.y});
         }
     }
-    return false;
+
+	if(hit_points.size() <= 0)
+	{
+		return false;
+	}
+
+	std::sort(hit_points.begin(), hit_points.end(), [&origin](Vector2 a, Vector2 b) {
+		return Vector2DistanceSqr(a, origin) < Vector2DistanceSqr(b, origin);
+	});
+
+	if(collision_point != nullptr)
+	{
+		*collision_point = hit_points[0];
+	}
+
+    return true;
 }
--- boids/main.cpp
+++ boids/main.cpp
@@ -1,15 +1,17 @@
 #include <base_definitions.hpp>
 #include <base_processors.hpp>
 #include <boids.hpp>
-#include <raymath.h>
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 
 bool random_raycast(entt::registry &registry, Vector2 *collision_point,
                     Vector2 *random_screen_position_1,
                     Vector2 *random_screen_position_2) {
+
     *random_screen_position_1 =
         Vector2{(float)GetRandomValue(0, 800), (float)GetRandomValue(0, 600)};
+
     *random_screen_position_2 =
         Vector2{(float)GetRandomValue(0, 800), (float)GetRandomValue(0, 600)};
 
@@ -19,8 +21,82 @@ bool random_raycast(entt::registry &registry, Vector2 *collision_point,
                    500, collision_point);
 }
 
+void create_screen_walls(entt::registry &registry) {
+    int width = GetScreenWidth();
+    int height = GetScreenHeight();
+
+    int horizontal_lenght = width;
+    int vertical_lenght = height;
+
+    auto top_wall = registry.create();
+    auto bottom_wall = registry.create();
+    auto left_wall = registry.create();
+    auto right_wall = registry.create();
+
+    std::vector<Vector2> horizontal_corners;
+    horizontal_corners.resize(4);
+
+    std::vector<Vector2> vertical_corners;
+    vertical_corners.resize(4);
+
+    rect_collider horizontal_collider(
+        false, Vector2{static_cast<float>(horizontal_lenght), 5});
+    rect_collider vertical_collider(
+        false, Vector2{5, static_cast<float>(vertical_lenght)});
+
+    horizontal_collider.generate_conners(horizontal_corners);
+    vertical_collider.generate_conners(vertical_corners);
+
+    registry.emplace<transform>(
+        top_wall, transform{Vector2{width / 2.0f, 0}, Vector2{1, 0}});
+    registry.emplace<rect_collider>(top_wall, horizontal_collider);
+    registry.emplace<renderable>(top_wall,
+                                 renderable{BLUE, 1, horizontal_corners});
+
+    registry.emplace<transform>(
+        bottom_wall,
+        transform{Vector2{width / 2.0f, static_cast<float>(height)},
+                  Vector2{1, 0}});
+    registry.emplace<rect_collider>(bottom_wall, horizontal_collider);
+    registry.emplace<renderable>(bottom_wall,
+                                 renderable{BLUE, 1, horizontal_corners});
+
+    registry.emplace<transform>(
+        left_wall, transform{Vector2{0, height / 2.0f}, Vector2{1, 0}});
+    registry.emplace<rect_collider>(left_wall, vertical_collider);
+    registry.emplace<renderable>(left_wall,
+                                 renderable{BLUE, 1, vertical_corners});
+
+    registry.emplace<transform>(
+        right_wall, transform{Vector2{static_cast<float>(width), height / 2.0f},
+                              Vector2{1, 0}});
+    registry.emplace<rect_collider>(right_wall, vertical_collider);
+    registry.emplace<renderable>(right_wall,
+                                 renderable{BLUE, 1, vertical_corners});
+}
+
+void random_block(entt::registry& registry)
+{
+	int width = GetScreenWidth();
+	int height = GetScreenHeight();
+
+	int horizontal_lenght = GetRandomValue(50, 200);
+	int vertical_lenght = GetRandomValue(50, 200);
+
+	std::vector<Vector2> corners;
+	corners.resize(4);
+
+	rect_collider collider(false, Vector2{ static_cast<float>(horizontal_lenght), static_cast<float>(vertical_lenght) });
+	collider.generate_conners(corners);
+
+	auto block = registry.create();
+	registry.emplace<transform>(block, transform{ Vector2{static_cast<float>(GetRandomValue(0, width)), static_cast<float>(GetRandomValue(0, height))}, Vector2{1, 0} });
+	registry.emplace<rect_collider>(block, collider);
+	registry.emplace<renderable>(block, renderable{ BLUE, 1, corners});
+}
+
 int main() {
-    InitWindow(800, 600, "Hello World");
+    InitWindow(800, 600, "BOIDS");
 
     entt::registry registry = entt::registry();
 
@@ -29,30 +105,12 @@ int main() {
 
     entt::scheduler render_scheduler;
     render_scheduler.attach<render_process>(registry);
-	render_scheduler.attach<vision_process>(registry);
+    render_scheduler.attach<vision_process>(registry);
 
     boids::create_n_boids(registry, 100, Vector2{400, 300}, 100);
 
-    Vector2 extents{150, 100};
-    std::vector<Vector2> corners;
-    corners.resize(4);
-
-    std::cout << "Corner size: " << corners.size() << "\n";
-
-    generate_rect_conners(extents, corners);
-
-    std::cout << "Corner size: " << corners.size() << "\n";
-
-    auto collision_entity = registry.create();
-
-	Vector2 rect_direction = Vector2Normalize(Vector2{static_cast<float>(GetRandomValue(-100, 100)), static_cast<float>(GetRandomValue(-100, 100))});
-    registry.emplace<transform>(collision_entity,
-                                transform{Vector2{400, 300}, rect_direction});
-
-    registry.emplace<rect_collider>(collision_entity,
-                                    rect_collider(false, extents));
-    registry.emplace<renderable>(collision_entity,
-                                 renderable(BLUE, 10, corners));
+    create_screen_walls(registry);
+	random_block(registry);
 
     Vector2 random_screen_position_1;
     Vector2 random_screen_position_2;
@@ -81,13 +139,16 @@ int main() {
             DrawCircleV(collision_point, 20, RED);
         }
 
-		auto direction = Vector2Scale(Vector2Normalize(Vector2Subtract(random_screen_position_2,
-														 random_screen_position_1)), 500);
-        DrawLineEx(random_screen_position_1, Vector2Add(random_screen_position_1,direction), 1.0f,
+        auto direction = Vector2Scale(
+            Vector2Normalize(Vector2Subtract(random_screen_position_2,
+                                             random_screen_position_1)),
+            500);
+        DrawLineEx(random_screen_position_1,
+                   Vector2Add(random_screen_position_1, direction), 1.0f,
                    intercepted ? RED : GREEN);
 
-		DrawCircleV(random_screen_position_1, 5, BLUE);
-		DrawCircleV(random_screen_position_2, 5, YELLOW);
+        DrawCircleV(random_screen_position_1, 5, BLUE);
+        DrawCircleV(random_screen_position_2, 5, YELLOW);
 
         EndDrawing();
     }
